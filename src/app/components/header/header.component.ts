fe513af30ebc2f4fbecd1b0a66ff84ceb1c35053
FelipeGuglielmeli <feliprimo@hotmail.com>
2023-06-23 15:28:14 -0300

    Ajustes finais

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit, Output, EventEmitter } from '@angular/core';
-import {DomSanitizer} from '@angular/platform-browser';
-import {MatIconRegistry, MatIconModule} from '@angular/material/icon';
+import { DomSanitizer } from '@angular/platform-browser';
+import { MatIconRegistry } from '@angular/material/icon';
 
 const THUMBUP_ICON =
   `
